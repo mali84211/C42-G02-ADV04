a37edfc13514149ad83b4bfa7a12fee474280322
mali84211 <mali84211@gmail.com>
2024-08-08 18:17:11 +0300

    Event EX1

--- C42-G02-ADV04/Program.cs
+++ C42-G02-ADV04/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace C42_G02_ADV04
+﻿using System.ComponentModel;
+
+namespace C42_G02_ADV04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region v1
+
+            //Ball ball = new Ball() { ID=1};
+            //Console.WriteLine(ball);
+            //ball.Location = new location() {x=1,y=1,z=1};   
+            //Console.WriteLine(ball);
+            //player p1 = new player() { playername="mo",teamname="miami"};
+            //player p2 = new player() { playername = "aa", teamname = "miami" };
+            //player p3 = new player() { playername = "dd", teamname = "barchlona" };
+            //player p4 = new player() { playername = "ww", teamname = "barchlona" };
+            //refree r1 = new refree() { Refree = "xx" };
+            //ball.Location = new location() { x = 1, y = 1, z = 1 };
+            //ball.BalllocatinChanged += p1.Run;
+            //ball.BalllocatinChanged += p2.Run;
+            //ball.BalllocatinChanged += p3.Run;
+            //ball.BalllocatinChanged += p4.Run;
+            //ball.BalllocatinChanged += r1.Run;
+            //ball.Location = new location() { x = 1, y = 1, z = 1 };
+            //ball.BalllocatinChanged += p1.Run;
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
         }
     }
 }
